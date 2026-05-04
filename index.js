1ccfd9638f553793469d24d8d87586c763822c3e
cunymerlin <esmerlin.lorareyes@lc.cuny.edu>
2026-05-04 18:34:41 -0400

    full stack app - express api with postgres

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const db = require('./queries')
 const port = 3000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
--- queries.js
+++ queries.js
@@ -16,28 +16,20 @@ const getUsers = (request, response) => {
   })
 }
 
-const getUserById = (request, response) => {
-  const id = parseInt(request.params.id)
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) throw error
-    response.status(200).json(results.rows)
-  })
-}
-
 const createUser = (request, response) => {
   const { name, email } = request.body
   pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
     if (error) throw error
-    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+    response.status(201).json(results.rows[0])
   })
 }
 
 const updateUser = (request, response) => {
   const id = parseInt(request.params.id)
   const { name, email } = request.body
-  pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3', [name, email, id], (error, results) => {
+  pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [name, email, id], (error, results) => {
     if (error) throw error
-    response.status(200).send(`User modified with ID: ${id}`)
+    response.status(200).json(results.rows[0])
   })
 }
 
@@ -45,14 +37,8 @@ const deleteUser = (request, response) => {
   const id = parseInt(request.params.id)
   pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
     if (error) throw error
-    response.status(200).send(`User deleted with ID: ${id}`)
+    response.status(200).json({ id })
   })
 }
 
-module.exports = {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-}
+module.exports = { getUsers, createUser, updateUser, deleteUser }
